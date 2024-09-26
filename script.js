44e14760b10c31ceb35c231c50cb151ae47fd681
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-09-26 02:42:03 +0100

    First commit.

---
+++ script.js
@@ -0,0 +1,52 @@
+// function add() {
+//     const displayElement = document.getElementById('answer');
+//     const num1 = Number(document.getElementById('fnum').value);
+//     const num2 = Number(document.getElementById('snum').value);
+
+//     numAdd = num1 + num2
+//     // return numAdd
+//     displayElement.innerHTML = `Answer: ${numAdd}`
+//     console.log(numAdd)
+    
+// }
+
+
+// function sub() {
+//     const num1 = document.getElementById('fnum').value;
+//     const num2 = document.getElementById('snum').value;
+
+//     numSub = num1 - num2
+//     // return numSub
+//     console.log(numSub)
+// }
+
+// function mul() {
+//     const num1 = document.getElementById('fnum').value;
+//     const num2 = document.getElementById('snum').value;
+
+//     numMul = num1 * num2
+//     // return numMul
+//     console.log(numMul)
+// }
+
+// function div() {
+//     const num1 = document.getElementById('fnum').value;
+//     const num2 = document.getElementById('snum').value;
+
+//     numDiv = num1 / num2
+//     // return numDiv
+//     console.log(numDiv)
+// }
+
+// function checkTest() {
+//     const displayElement = document.getElementById('answer');
+//     displayElement.innerHTML = `Answer: `
+    
+// }
+
+
+function signUp(){
+    const displayElement = document.getElementById("displayName")
+    console.log(displayElement.innerHTML)
+    console.log(2)
+}
