0dbff64d6879f4dff51418bba6260eaa40d9c489
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-05-08 10:38:21 +0100

    Added the install button.

--- script.js
+++ script.js
@@ -36,3 +36,34 @@ function clearResult() {
     document.getElementById('snum').value = "";
     document.getElementById('demo').innerHTML = "";
 }
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault();
+  // Save the event for later use
+    deferredPrompt = e;
+  // Show the install button
+    const installBtn = document.getElementById("installBtn");
+    installBtn.style.display = "block";
+
+    installBtn.addEventListener("click", () => {
+        // Hide the install button
+        installBtn.style.display = "none";
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("User accepted the install prompt");
+            } else {
+                console.log("User dismissed the install prompt");
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+window.addEventListener("appinstalled", () => {
+    console.log("PWA was installed");
+});
